d1cca471a219b0c7239a009ea1fa830ec08bbf2a
farahzuot <farahz997@eng.hu.edu.jo>
2020-10-29 21:34:22 +0300

    lab9

--- schema.sql
+++ schema.sql
@@ -5,5 +5,6 @@ CREATE TABLE location(
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
     latitude NUMERIC(10, 7),
-    longitude NUMERIC(10, 7)
+    longitude NUMERIC(10, 7),
+    YELP=OzqBW8iBBvU72rxL0MyVFDCTRVuSBD29ahJW4cXw1OXMhOI7Zxlb_3tHhWlTuLJ2xAR9C1lOkq1v6Ep7Q9Ursz2pmZni8dwnXZH2LRlalfHfS6YZI1qFVyMhYgCbX3Yx
 );
